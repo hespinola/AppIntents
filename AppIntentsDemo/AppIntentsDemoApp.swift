3f4ce8614805315c6c999824113ba67396be0d3a
Humberto Espinola <hespinola@etsy.com>
2025-07-08 11:57:52 -0600

    Initial Commit

---
+++ AppIntentsDemo/AppIntentsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIntentsDemoApp.swift
+//  AppIntentsDemo
+//
+//  Created by Humberto Espinola on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AppIntentsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppIntentsDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppIntentsDemo
+//
+//  Created by Humberto Espinola on 7/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
